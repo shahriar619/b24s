2f1151a85f7f0cf3c614a6d23434756063abd0b7
shahriar619 <shahriaralshahin80@gmail.com>
2023-01-04 22:33:58 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+// MIXITUP
+var mixer = mixitup('.gallery');
+// WOW
+wow = new WOW(
+    {
+    mobile:       folse,       // default
+
+  }
+  )
+  wow.init();
+
+  // MENU 
+  function openMenu (){
+    document.getElementById('menu-container').style.width = "100%";
+  }
+  function closeMenu (){
+    document.getElementById('menu-container').style.width = "0%";
+  }
